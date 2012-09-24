0b4cb22308cebc022d9160623b8a87830af03cb5
Gregory <gregpetrop@gmail.com>
2012-09-24 11:11:26 -0600

    added files

---
+++ transpose.cpp
@@ -0,0 +1,12 @@
+/*
+ * Gregory Petropoulos
+ *
+ * This is my matrix transpose program for Assignment 3
+ *
+ * To compile:  mpicxx -g -Wall -std=c99 -o transpose transpose.cpp
+ * To run:  mpiexec -n 8 ./transpose
+ *          <> -> mandatory
+ *          [] -> optional
+ */
+
+
